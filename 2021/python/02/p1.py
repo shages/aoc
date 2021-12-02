d8008e6ca65ae8b5e26e08da7bade1030d9c03bd
shages <eronshagen@gmail.com>
2021-12-01 21:11:37 -0800

    2021-02-python

---
+++ 2021/python/02/p1.py
@@ -0,0 +1,59 @@
+import itertools
+import re
+
+
+def read_input():
+    with open("input") as f:
+        return f.read()
+
+
+def parse_lines():
+    return read_input().splitlines()
+
+
+def parse_lines_int():
+    return [int(line) for line in parse_lines()]
+
+
+def parse_lines_float():
+    return [float(line) for line in parse_lines()]
+
+
+def parse_chars():
+    return read_input().rstrip()
+
+
+def parse_words():
+    return read_input().rstrip().split()
+
+
+def parse_digits():
+    return [int(c) for c in parse_chars()]
+
+
+def parse_space_ints():
+    return [int(c) for c in parse_chars().split()]
+
+
+def parse_space_floats():
+    return [float(c) for c in parse_chars().split()]
+
+
+def main():
+    lines = parse_lines()
+    h = 0
+    d = 0
+    for line in lines:
+        vals = line.split()
+        if vals[0] == "forward":
+            h += int(vals[1])
+        elif vals[0] == "down":
+            d += int(vals[1])
+        elif vals[0] == "up":
+            d -= int(vals[1])
+
+    print(d * h)
+
+
+if __name__ == "__main__":
+    main()
